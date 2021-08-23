72f5fc941eca622a295318151202a15a821e239d
Adrien <bartolini.adrien@yahoo.fr>
2021-08-23 17:06:19 +0200

    after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_141621) do
+ActiveRecord::Schema.define(version: 2021_08_23_143424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "investees", force: :cascade do |t|
+    t.string "SIRET"
+    t.string "company_name"
+    t.string "NAF"
+    t.string "address"
+    t.string "activity"
+    t.string "president"
+    t.string "managing_director"
+    t.integer "shares"
+    t.text "table_cap"
+    t.integer "turnover_n_2"
+    t.integer "turnover_n_1"
+    t.integer "turnover_n"
+    t.integer "growth_n_2_n_1"
+    t.string "growth_n_1_n"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "investments", force: :cascade do |t|
     t.float "amount"
@@ -39,25 +79,6 @@ ActiveRecord::Schema.define(version: 2021_08_23_141621) do
     t.index ["user_id"], name: "index_investors_on_user_id"
   end
 
-  create_table "investees", force: :cascade do |t|
-    t.string "SIRET"
-    t.string "company_name"
-    t.string "NAF"
-    t.string "address"
-    t.string "activity"
-    t.string "president"
-    t.string "managing_director"
-    t.integer "shares"
-    t.text "table_cap"
-    t.integer "turnover_n_2"
-    t.integer "turnover_n_1"
-    t.integer "turnover_n"
-    t.integer "growth_n_2_n_1"
-    t.string "growth_n_1_n"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.integer "valuation"
     t.integer "conversion_rate"
@@ -68,27 +89,6 @@ ActiveRecord::Schema.define(version: 2021_08_23_141621) do
     t.string "financing_thesis"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "users", force: :cascade do |t|
