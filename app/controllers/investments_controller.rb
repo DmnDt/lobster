2a3e1833fae73fbdaf80ffe5489ca139083bbee8
nasreddine ghribi <nas.ghribi@gmail.com>
2021-08-24 13:02:34 +0200

    created investment controller, need to be tested with the seed

---
+++ app/controllers/investments_controller.rb
@@ -0,0 +1,16 @@
+class InvestmentsController < ApplicationController
+   def create
+    @investment.new(booking_params)
+    @project = Project.find(params[:project_id])
+    @investor = Investor.find(params[:investor_id])
+    @investment.project = @project
+    @investment.investor = @investor
+    @investment.save # need to add the redirect
+  end
+
+  def mark_as_ter
+    @investment = Investment.find(params[:id])
+    @investment.update(status: 1)
+    # redirect_to dashboard_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_155435) do
 
   create_table "investments", force: :cascade do |t|
     t.float "amount"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "investor_id"
