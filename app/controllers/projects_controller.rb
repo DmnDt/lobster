60da16a566665d5032fba7c4e6adbe7cc083a843
Adrien <bartolini.adrien@yahoo.fr>
2021-08-24 14:53:48 +0200

    seed with 2 entrys

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,31 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+    @user = current_user
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @user = current_user
+    @project.user = @user
+    if @project.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def index
     @projects= Project.all
   end
+
   def show
     @project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:valuation, :conversion_rate, :coupon, :conversion_date, :status, :total_amount, :financing_thesis, :investee_id)
   end
 end
