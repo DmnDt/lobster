289a23d106450b033b037b8e37a734871616cd71
Adrien <bartolini.adrien@yahoo.fr>
2021-08-25 11:54:01 +0200

    adding name to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :investee
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_synopsis,
+                  against: [ :title, :synopsis ],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_090519) do
+ActiveRecord::Schema.define(version: 2021_08_25_094936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_090519) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "investee_id"
     t.string "industry"
+    t.string "name"
     t.index ["investee_id"], name: "index_projects_on_investee_id"
   end
 
