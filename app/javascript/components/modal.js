ee0ff3a62fe87d03771a91376296c4cae941a6c0
Damien Douat <damiendouat@gmail.com>
2021-08-26 14:44:17 +0200

    proect show

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -19,8 +19,10 @@ function modal() {
   const button_validate = document.getElementById("button_validate");
   const submit_form = document.getElementById("new_investment");
   // console.log(new_investment)
+  if (button_validate) {
   button_validate.addEventListener('click', () => {
     submit_form.submit();
-  })
+  });
+  }
 }
 export {modal};
