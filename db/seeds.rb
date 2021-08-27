f7073bfc06b07c00069898eb962918f541720dd5
Adrien <bartolini.adrien@yahoo.fr>
2021-08-27 11:13:35 +0200

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ Investor.destroy_all
 Project.destroy_all
 
 steve_jobs = User.create!(full_name: 'Steve Jobs', email: 'steve-jobs@apple.com', password: 'azerty', category: 'investee')
-photo = URI.open("https://res.cloudinary.com/dgkikk5i4/image/upload/v1629811189/apple-steve-jobs-big1-1280x720_hdfvpw.jpg")
-steve_jobs.photo.attach(io: photo, filename: "JP2", content_type: "image/jpg")
+# photo = asset-url("Apple")
+steve_jobs.photo.attach(io: File.open('app/assets/images/Apple.png'), filename: "JP2", content_type: "image/png")
 steve_jobs.save!
 puts "#{steve_jobs.full_name}"
 
 elon_musk = User.create!(full_name: 'Elon Musk', email: 'elon-musk@tesla.com', password: 'azerty', category: 'investee')
-photo = URI.open("https://res.cloudinary.com/dgkikk5i4/image/upload/v1629811194/Elon_Musk_Royal_Society_mk09mp.jpg")
-elon_musk.photo.attach(io: photo, filename: "JP2", content_type: "image/jpg")
+# photo = asset-url("Tesla")
+elon_musk.photo.attach(io: File.open('app/assets/images/Tesla.png'), filename: "JP2", content_type: "image/png")
 elon_musk.save!
 puts "#{elon_musk.full_name}"
 
@@ -36,3 +36,7 @@ project_steve_jobs.documents.attach(io: document1, filename: 'Bilan.pdf', conten
 document2 = URI.open("https://res.cloudinary.com/dgkikk5i4/image/upload/v1629885783/sample_h30d1q.pdf")
 project_steve_jobs.documents.attach(io: document2, filename: 'compte de résultat.pdf', content_type: 'application/pdf')
 project_steve_jobs.save!
+
+
+# stevejobsphoto = https://res.cloudinary.com/dgkikk5i4/image/upload/v1629811189/apple-steve-jobs-big1-1280x720_hdfvpw.jpg
+# elonmuskphoto = https://res.cloudinary.com/dgkikk5i4/image/upload/v1629811194/Elon_Musk_Royal_Society_mk09mp.jpg
