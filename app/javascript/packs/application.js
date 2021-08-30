2e7139bd7173b3cf8cd3e2d85497f78e127ee402
Adrien <bartolini.adrien@yahoo.fr>
2021-08-30 09:51:55 +0200

    idk

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,6 +30,7 @@ import { modal } from "../components/modal";
 // import { initSelect2 } from '../components/init_select2';
 import { initSelect2 } from '../plugins/init_select2';
 
+// probablement event listener se desactive en clickant sur search
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
