c5d169e445fa2ddec271561ab24bd3c87960f2f6
nasreddine ghribi <nas.ghribi@gmail.com>
2021-08-31 16:09:19 +0200

    added dynamic name and email to docusign

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -32,8 +32,8 @@ class ProjectsController < ApplicationController
 
   def data_room
     @project = Project.find(params[:id])
-    @url_docusign = DocusignService.new.get_url["url"]
-    # raise
+    @url_docusign = DocusignService.new(doc_user:current_user).get_url["url"]
+
   end
 
   private
