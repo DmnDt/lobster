5de2af1ff8603ddabb594217d0b724e5dbb761ed
nasreddine ghribi <nas.ghribi@gmail.com>
2021-09-02 16:46:43 +0200

    fixed the project form

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,8 @@ class Project < ApplicationRecord
   has_many_attached :documents
   has_one :user, through: :investee
   has_many :investments
-
+  INDUSTRIES =['Tech', 'E-commerce', 'IT finance', 'Deep tech', 'Transformation industrielle', 'Agroalimentaire', 'Téléphonie', 'Banque en ligne' ]
+  OBJECTIFS =['Financer des recrutements', "Location d'équipements", "Ouvrir de nouveaux bureaux", "Financer une campagne de communication", "Acheter des machines"]
   include PgSearch::Model
   pg_search_scope :search_by_name_and_industry,
                   against: [ :name, :industry ], # Search by project name and/or industry
